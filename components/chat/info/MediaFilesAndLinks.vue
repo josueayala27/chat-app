c1740305c7ae93ec15babdea53c701fa260abae7
Josué Ayala <ayalavalencia11@gmail.com>
2025-02-28 05:05:28 +0000

    refactor chat info components; adjust gap properties in Aside and container components for improved layout consistency

--- components/chat/info/MediaFilesAndLinks.vue
+++ components/chat/info/MediaFilesAndLinks.vue
@@ -9,7 +9,7 @@
     </span>
   </div>
 
-  <div class="grid grid-cols-3 gap-0.5">
-    <div v-for="item in 9" :key="item" role="img" class="bg-slate-100 w-full aspect-square rounded-lg" />
+  <div class="grid grid-cols-3 gap-1">
+    <div v-for="item in 9" :key="item" role="img" class="bg-slate-100 w-full aspect-square" />
   </div>
 </template>
