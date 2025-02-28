d0bdf58c7d00c183c7ea5dc66f604e3e242d0a0b
Josué Ayala <ayalavalencia11@gmail.com>
2025-02-28 22:12:52 +0000

    refactor chat layout; enhance structure by replacing nested divs with v-for loops for improved readability and organization

--- pages/[chat].vue
+++ pages/[chat].vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="flex-1 p-2">
-    <div class="flex gap-2">
-      <BaseAvatar />
-      <div class="flex flex-col gap-1">
-        <BaseFont class="text-sm text-slate-900 font-[500]" content="Josué Ayala" />
-        <div class="p-2 bg-sky-500 text-white rounded-r-lg rounded-bl-lg">
-          <BaseFont class="text-sm text-inherit" content="Hello there!" />
+  <div class="flex-1 p-2 overflow-auto">
+    <div class="flex flex-col gap-2">
+      <div v-for="item in 8" :key="item" class="flex gap-2">
+        <BaseAvatar />
+        <div class="flex flex-col gap-1">
+          <BaseFont class="text-sm text-slate-900 font-medium" content="Josué Ayala" />
+          <div class="flex flex-col gap-0.5">
+            <div v-for="_item in 3" :key="_item" class="p-2 bg-sky-500 text-white rounded-r-lg last:rounded-bl-lg">
+              <BaseFont class="text-sm text-inherit" content="Hello there!" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
