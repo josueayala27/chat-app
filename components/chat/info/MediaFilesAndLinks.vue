f40d1e922e29262a510bee25276afbadba43c7a3
Josué Ayala <ayalavalencia11@gmail.com>
2025-02-28 04:25:10 +0000

    refactor chat info components; extract media, files, and links section into new MediaFilesAndLinks component

---
+++ components/chat/info/MediaFilesAndLinks.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex gap-2 items-center text-sm">
+    <span
+      v-for="type in ['Media', 'Files', 'Links']"
+      :key="type"
+      class="px-3 py-1 rounded-full bg-sky-100 text-sky-600 font-medium cursor-pointer select-none"
+    >
+      {{ type }}
+    </span>
+  </div>
+
+  <div class="grid grid-cols-3 gap-0.5">
+    <div v-for="item in 9" :key="item" role="img" class="bg-slate-100 w-full aspect-square rounded-lg" />
+  </div>
+</template>
--- components/chat/info/Section.vue
+++ components/chat/info/Section.vue
@@ -18,19 +18,7 @@ const isOpen = ref(false)
     </div>
 
     <div v-if="isOpen" class="px-2 flex flex-col gap-2 overflow-hidden">
-      <div class="flex gap-2 items-center text-sm">
-        <span
-          v-for="type in ['Media', 'Files', 'Links']"
-          :key="type"
-          class="px-3 py-1 rounded-full bg-sky-100 text-sky-600 font-medium cursor-pointer select-none"
-        >
-          {{ type }}
-        </span>
-      </div>
-
-      <div class="grid grid-cols-3 gap-0.5">
-        <div v-for="item in 9" :key="item" role="img" class="bg-slate-100 w-full aspect-square rounded-lg" />
-      </div>
+      <slot />
     </div>
   </div>
 </template>
