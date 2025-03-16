36f015e4f9e90469a8b702c74c3ef9f231e005d3
Josué Ayala <ayalavalencia11@gmail.com>
2025-03-16 00:22:52 +0000

    feat: update Media component image source for improved visual quality

--- components/chat/messages/Media.vue
+++ components/chat/messages/Media.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="text-white rounded-r-lg last:rounded-bl-lg inline-flex overflow-hidden max-w-[40%] h-auto cursor-pointer">
-    <img src="https://plus.unsplash.com/premium_photo-1675355675360-a0cfa025f682?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+    <img src="https://images.unsplash.com/photo-1728588266992-a50e7adc55c2?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
   </div>
 </template>
