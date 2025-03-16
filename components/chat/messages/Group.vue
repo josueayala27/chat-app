1460d232e7a353ef906dca7c7816d1d2f4002f24
Josué Ayala <ayalavalencia11@gmail.com>
2025-03-16 00:01:45 +0000

    feat: refactor chat message components by consolidating Item.vue into Text.vue and updating Group.vue to use new Text component

--- components/chat/messages/Group.vue
+++ components/chat/messages/Group.vue
@@ -4,9 +4,9 @@
     <div class="flex flex-col gap-1">
       <BaseFont class="text-sm text-slate-900 font-medium" content="Josué Ayala" />
       <div class="flex flex-col gap-0.5">
-        <div v-for="_item in 1" :key="_item" class="p-2 bg-sky-500 text-white rounded-r-lg last:rounded-bl-lg">
-          <BaseFont class="text-sm text-inherit" content="Hello there!" />
-        </div>
+        <template v-for="_item in 4" :key="_item">
+          <ChatMessagesText />
+        </template>
       </div>
     </div>
   </div>
---
+++ components/chat/messages/Text.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="p-2 bg-sky-500 text-white rounded-r-lg last:rounded-bl-lg">
+    <BaseFont class="text-sm text-inherit" content="Hello there!" />
+  </div>
+</template>
