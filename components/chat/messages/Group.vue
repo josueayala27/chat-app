8ba4c16b2c6b092a3d11378aed8d1c9c2d33a278
Josué Ayala <ayalavalencia11@gmail.com>
2025-03-16 00:18:22 +0000

    feat: add Media component and update Group to render ChatMessagesMedia alongside ChatMessagesText

--- components/chat/messages/Group.vue
+++ components/chat/messages/Group.vue
@@ -3,9 +3,10 @@
     <BaseAvatar />
     <div class="flex flex-col gap-1">
       <BaseFont class="text-sm text-slate-900 font-medium" content="Josué Ayala" />
-      <div class="flex flex-col gap-0.5">
+      <div class="flex flex-col gap-0.5 items-start">
         <template v-for="_item in ['text', 'text', 'media', 'text']" :key="_item">
           <ChatMessagesText v-if="_item.includes('text')" />
+          <ChatMessagesMedia v-if="_item.includes('media')" />
         </template>
       </div>
     </div>
