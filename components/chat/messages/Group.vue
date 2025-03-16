9bd48d850998394ffc8497a337c1a0341b41eccc
Josué Ayala <ayalavalencia11@gmail.com>
2025-03-16 00:09:02 +0000

    feat: update Group component to conditionally render ChatMessagesText based on item type

--- components/chat/messages/Group.vue
+++ components/chat/messages/Group.vue
@@ -4,8 +4,8 @@
     <div class="flex flex-col gap-1">
       <BaseFont class="text-sm text-slate-900 font-medium" content="Josué Ayala" />
       <div class="flex flex-col gap-0.5">
-        <template v-for="_item in 4" :key="_item">
-          <ChatMessagesText />
+        <template v-for="_item in ['text', 'text', 'media', 'text']" :key="_item">
+          <ChatMessagesText v-if="_item.includes('text')" />
         </template>
       </div>
     </div>
