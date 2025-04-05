06f51fc603c26ea39ff68e0ddd67868ea89159e5
Josué Ayala <ayalavalencia11@gmail.com>
2025-04-05 22:42:28 +0000

    style: format Avatar component template for improved readability

--- components/base/Avatar.vue
+++ components/base/Avatar.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-withDefaults(defineProps<{ size?: number | string }>(), {
-  size: 48,
-})
+withDefaults(defineProps<{ size?: number | string }>(), { size: 48 })
 </script>
 
 <template>
-  <div :style="{ '--avatar-size': `${size}px` }" class="size-(--avatar-size) bg-slate-100 rounded-full group-hover:bg-slate-200 shrink-0" />
+  <div
+    :style="{ '--avatar-size': `${size}px` }"
+    class="size-(--avatar-size) bg-slate-100 rounded-full group-hover:bg-slate-200 shrink-0"
+  />
 </template>
