956b65b68c31808c5c7a13676174f3e985510400
Josué Ayala <ayalavalencia11@gmail.com>
2025-04-05 23:23:12 +0000

    style: update slot definitions in Group component for consistency and improve Media and Text components' styling

--- components/chat/messages/Group.vue
+++ components/chat/messages/Group.vue
@@ -4,29 +4,38 @@ import { tv } from 'tailwind-variants'
 const props = defineProps<{ isOwn: boolean }>()
 
 const ui = tv({
-  slots: { root: 'flex gap-2', content: 'flex flex-col' },
+  slots: {
+    root: 'flex gap-2',
+    container: 'flex flex-col',
+  },
   variants: {
     isOwn: {
-      true: { root: 'flex-row-reverse', content: 'items-end' },
-      false: { root: 'flex-row', content: 'items-start' },
+      true: {
+        root: 'flex-row-reverse',
+        container: 'items-end',
+      },
+      false: {
+        root: 'flex-row',
+        container: 'items-start',
+      },
     },
   },
 })
 
-const { root, content } = ui({ isOwn: props.isOwn })
+const { root, container } = ui({ isOwn: props.isOwn })
 </script>
 
 <template>
   <div :class="root()">
     <BaseAvatar />
 
-    <div :class="content({ class: 'gap-1' })">
+    <div :class="container({ class: 'gap-1' })">
       <BaseFont
         class="text-sm text-slate-900 font-medium"
         :content="isOwn ? 'Tú' : 'Josué Ayala'"
       />
 
-      <div :class="content({ class: 'gap-0.5' })">
+      <div :class="container({ class: 'gap-0.5' })">
         <template v-for="(_item, i) in ['text', 'text', 'media', 'text']" :key="i">
           <ChatMessagesText v-if="_item.includes('text')" :is-own="isOwn" />
           <ChatMessagesMedia v-if="_item.includes('media')" :is-own="isOwn" />
