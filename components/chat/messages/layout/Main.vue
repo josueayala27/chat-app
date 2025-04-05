3f92cfc3b9a8b88dbd8f2ea50b9a0786866ff034
Josué Ayala <ayalavalencia11@gmail.com>
2025-04-05 22:35:11 +0000

    feat: refactor chat layout by moving ChatMessagesGroup into ChatMessagesLayoutMain and adding Main component

---
+++ components/chat/messages/layout/Main.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="p-2 overflow-auto scrollbar-hidden flex-1 flex flex-col gap-2 bg-red-300">
+    <slot />
+  </div>
+</template>
--- pages/[chat].vue
+++ pages/[chat].vue
@@ -1,13 +1,9 @@
 <template>
   <div class="flex flex-col divide-y divide-slate-200 flex-1">
     <ChatMessagesLayoutHeader />
-
-    <div class="p-2 overflow-auto scrollbar-hidden flex-1">
-      <div class="flex flex-col gap-2">
-        <ChatMessagesGroup v-for="item in 8" :key="item" />
-      </div>
-    </div>
-
+    <ChatMessagesLayoutMain>
+      <ChatMessagesGroup v-for="item in 8" :key="item" />
+    </ChatMessagesLayoutMain>
     <ChatMessagesLayoutFooter />
   </div>
 
