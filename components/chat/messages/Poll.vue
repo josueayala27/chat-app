9f0906c6e00f6e298b42cfb576b3699b883f26e3
Josué Ayala <ayalavalencia11@gmail.com>
2025-04-06 00:01:16 +0000

    style: restructure Poll component layout for improved readability

--- components/chat/messages/Poll.vue
+++ components/chat/messages/Poll.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="p-2 bg-sky-500 text-white rounded-r-lg last:rounded-bl-lg inline-flex flex-col gap-4 w-[20rem]">
-    <BaseFont class="text-sm text-inherit font-semibold" content="THIS IS MY POLL" />
+  <div class="bg-sky-500 text-white rounded-r-lg last:rounded-bl-lg flex-col inline-flex w-[20rem]">
+    <div class="p-2 flex flex-col gap-4">
+      <BaseFont class="text-sm text-inherit font-semibold" content="THIS IS MY POLL" />
 
-    <div v-for="item in 3" :key="item" class="flex flex-col gap-2 cursor-pointer">
-      <div class="flex items-center gap-2">
-        <div class="size-3 bg-sky-400 rounded-full" />
-        <BaseFont class="text-sm text-inherit" :content="`Poll Option ${item + 1}`" />
+      <div v-for="item in 3" :key="item" class="flex flex-col gap-2 cursor-pointer">
+        <div class="flex items-center gap-2">
+          <div class="size-3 bg-sky-400 rounded-full" />
+          <BaseFont class="text-sm text-inherit" :content="`Poll Option ${item + 1}`" />
+        </div>
+
+        <div class="h-2 rounded-full bg-sky-400" />
       </div>
-
-      <div class="h-2 rounded-full bg-sky-400" />
     </div>
+
+    <button class="w-full cursor-pointer hover:text-white/80 p-2">
+      <BaseFont class="text-sm text-inherit" content="View votes" />
+    </button>
   </div>
 </template>
