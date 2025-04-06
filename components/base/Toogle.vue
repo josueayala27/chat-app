d9c5864383ce0cc6a9c3afa7806e9715fb7db9e2
Josué Ayala <ayalavalencia11@gmail.com>
2025-04-06 22:53:16 +0000

    style: update Toogle component styles for improved visual consistency

--- components/base/Toogle.vue
+++ components/base/Toogle.vue
@@ -9,12 +9,12 @@ const computedDataState = computed(() => {
   <button
     :aria-checked="model"
     :data-state="computedDataState"
-    class="inline-flex items-center cursor-pointer border-2 border-transparent shrink-0 rounded-full data-[state=unchecked]:bg-slate-100 data-[state=checked]:bg-blue-500 duration-200 w-9"
+    class="inline-flex items-center cursor-pointer border-2 border-transparent shrink-0 rounded-full duration-200 w-9 data-[state=unchecked]:bg-slate-200 data-[state=checked]:bg-blue-500"
     @click="model = !model"
   >
     <span
       :data-state="computedDataState"
-      class="group pointer-events-none rounded-full bg-white shadow-lg ring-0 duration-200 data-[state=unchecked]:translate-x-0 data-[state=unchecked]:rtl:-translate-x-0 flex items-center justify-center size-4 data-[state=checked]:translate-x-4 data-[state=checked]:rtl:-translate-x-4"
+      class="pointer-events-none rounded-full bg-white shadow-lg ring-0 duration-200 flex items-center justify-center size-4 data-[state=unchecked]:translate-x-0 data-[state=checked]:translate-x-4"
     />
   </button>
 </template>
