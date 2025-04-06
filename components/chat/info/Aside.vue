3a225f50784e792246e3d2f2dffa119bc820f396
Josué Ayala <ayalavalencia11@gmail.com>
2025-04-06 23:56:51 +0000

    style: correct spelling of "Clamp" to "Clap" in Message Sounds section

--- components/chat/info/Aside.vue
+++ components/chat/info/Aside.vue
@@ -42,7 +42,7 @@
       </ChatInfoSectionItem>
       <ChatInfoSectionItem title="Message Sounds">
         <button class="py-1 px-2 bg-slate-200/50 hover:bg-slate-200 rounded-lg cursor-pointer duration-200 flex items-center gap-2">
-          <BaseFont class="text-sm text-slate-700" content="Clamp" />
+          <BaseFont class="text-sm text-slate-700" content="Clap" />
           <Icon name="carbon:chevron-down" size="20px" />
         </button>
       </ChatInfoSectionItem>
