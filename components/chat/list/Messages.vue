9eb9be0b5e7fbc6ef88224437967d268a965f084
Josué Ayala <ayalavalencia11@gmail.com>
2025-04-06 22:15:12 +0000

    style: update Message interface to use string type for id and adjust message data accordingly

--- components/chat/list/Messages.vue
+++ components/chat/list/Messages.vue
@@ -1,16 +1,18 @@
-<script lang="ts" setup>
+<script lang="ts">
 interface Message {
-  id: number
+  id: string
   name: string
   message: string
 }
+</script>
 
+<script lang="ts" setup>
 const messages = ref<Message[]>([
-  { id: 1, name: 'Alice', message: 'Hello there!' },
-  { id: 2, name: 'Bob', message: 'How are you?' },
-  { id: 3, name: 'Charlie', message: 'Good morning!' },
-  { id: 4, name: 'David', message: 'What\'s up?' },
-  { id: 5, name: 'Eve', message: 'See you later!' },
+  { id: '378ae97b-d6c6-4ec4-bf39-2aaae75bd514', name: 'Alice', message: 'Hello there!' },
+  { id: '86b765de-b5ef-4334-a040-28bd1a46c5ad', name: 'Bob', message: 'How are you?' },
+  { id: '1a7895ee-d744-4f81-9aa9-c16f14463232', name: 'Charlie', message: 'Good morning!' },
+  { id: '5857f77a-f5cc-410c-9871-1d5651427d96', name: 'David', message: 'What\'s up?' },
+  { id: '4fd8a65d-de60-43ed-89bd-343359c9989f', name: 'Eve', message: 'See you later!' },
 ])
 </script>
 
