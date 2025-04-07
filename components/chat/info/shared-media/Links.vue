75f966c5d472b8be2241333d22a7beef60ff63bd
Josué Ayala <ayalavalencia11@gmail.com>
2025-04-07 01:56:39 +0000

    style: add v-for directive to Links component for dynamic rendering of items

--- components/chat/info/shared-media/Links.vue
+++ components/chat/info/shared-media/Links.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-2 flex items-start gap-2 cursor-pointer hover:bg-slate-50 group duration-200">
+  <div v-for="item in 2" :key="item" class="p-2 flex items-start gap-2 cursor-pointer hover:bg-slate-50 group duration-200">
     <div class="group-hover:bg-slate-300/50 bg-slate-100 size-10 shrink-0 rounded-lg duration-200" />
     <div class="flex flex-col">
       <BaseFont class="text-sm font-semibold" content="Online UUID Generator" />
