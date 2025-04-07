748e28360e4961f30654a097466c4233d8cc6845
Josué Ayala <ayalavalencia11@gmail.com>
2025-04-07 02:12:14 +0000

    style: update Files component with structured layout and improved hover effects

--- components/chat/info/shared-media/Files.vue
+++ components/chat/info/shared-media/Files.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    Files...
+  <div class="p-2 flex items-center gap-2 cursor-pointer hover:bg-slate-50 group duration-200">
+    <div class="group-hover:bg-slate-300/50 bg-slate-100 size-10 shrink-0 rounded-lg duration-200" />
+    <div class="flex flex-col">
+      <BaseFont class="text-sm font-semibold line-clamp-1" content="video-38364254-b049f6aa776ffc019d706b97b6c40af3.mp4" />
+      <BaseFont class="text-sm text-slate-700 line-clamp-2" content="1.8 MB - Today, 08:05 PM" />
+    </div>
   </div>
 </template>
