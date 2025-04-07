b7d112137ae462b3294fcbf5b4e45910276fddcd
Josué Ayala <ayalavalencia11@gmail.com>
2025-04-07 01:45:42 +0000

    style: update Links component with structured layout and improved hover effects

--- components/chat/info/shared-media/Links.vue
+++ components/chat/info/shared-media/Links.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
-    Links...
+  <div class="p-2 flex items-start gap-2 cursor-pointer hover:bg-slate-50 group duration-200">
+    <div class="group-hover:bg-slate-300/50 bg-slate-100 size-10 shrink-0 rounded-lg duration-200" />
+    <div class="flex flex-col">
+      <BaseFont class="text-sm font-semibold" content="Online UUID Generator" />
+      <BaseFont class="text-sm text-slate-700 line-clamp-2" content="Quickly and easily generate individual or bulk sets of universally unique identifiers (UUIDs)." />
+      <BaseFont class="text-sm text-blue-500 font-medium group-hover:underline" content="https://www.uuidgenerator.net" />
+    </div>
   </div>
 </template>
