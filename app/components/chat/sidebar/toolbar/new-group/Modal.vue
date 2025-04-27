10fec8f12302066c36be8bad4d8ecc9f33893d06
Josué Ayala <ayalavalencia11@gmail.com>
2025-04-27 01:58:03 +0000

    feat: implement multi-step group creation modal with navigation

--- app/components/chat/sidebar/toolbar/new-group/Modal.vue
+++ app/components/chat/sidebar/toolbar/new-group/Modal.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
 const isGroupModalOpen = defineModel<boolean>({ default: false })
+const currentStep = ref(1)
+
+function goToStepTwo() {
+  currentStep.value++
+}
 </script>
 
 <template>
@@ -8,7 +13,6 @@ const isGroupModalOpen = defineModel<boolean>({ default: false })
     :ui="{
       container: 'w-[442px]',
       header: 'flex flex-col gap-2',
-      body: 'items-center justify-center flex flex-col gap-4',
       footer: 'flex justify-end items-center gap-2',
     }"
   >
@@ -17,23 +21,15 @@ const isGroupModalOpen = defineModel<boolean>({ default: false })
       <BaseFont class="text-sm font-normal text-neutral-700" content="Gather your friends in a vibrant chat. Share moments and ideas in one fun conversation." />
     </template>
 
-    <BaseAvatar class="border-dashed border border-neutral-200 flex items-center justify-center text-neutral-500 cursor-pointer hover:bg-slate-200 duration-200" size="80">
-      <Icon size="24px" name="carbon:camera" />
-    </BaseAvatar>
-
-    <div class="flex flex-col gap-2 w-full">
-      <label class="text-sm text-neutral-700" for="test_uuid">Group name</label>
-      <div :style="{ '--input-width': '44px' }" class="flex items-center top-0 stick y w-full relative">
-        <input id="test_uuid" type="text" placeholder="Search..." class="w-full px-4 h-11 pr-3 bg-slate-100 border-slate-200 rounded-lg outline-none text-sm">
-      </div>
-    </div>
+    <SidebarToolbarNewGroupStepOne v-if="currentStep === 1" />
+    <SidebarToolbarNewGroupStepTwo v-else-if="currentStep === 2" />
 
     <template #footer>
       <button class="bg-slate-100 hover:bg-slate-200 rounded-lg h-9 px-4 flex items-center justify-center text-slate-700 cursor-pointer text-sm duration-200" @click="isGroupModalOpen = false">
         Close
       </button>
-      <button class="bg-sky-500 hover:bg-sky-600 rounded-lg h-9 px-4 flex items-center justify-center text-white cursor-pointer text-sm duration-200">
-        Create group
+      <button class="bg-sky-500 hover:bg-sky-600 rounded-lg h-9 px-4 flex items-center justify-center text-white cursor-pointer text-sm duration-200" @click="goToStepTwo">
+        Next
       </button>
     </template>
   </BaseModal>
---
+++ app/components/chat/sidebar/toolbar/new-group/StepTwo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="items-center justify-center flex flex-col gap-4">
+    <BaseAvatar class="border-dashed border border-neutral-200 flex items-center justify-center text-neutral-500 cursor-pointer hover:bg-slate-200 duration-200" size="80">
+      <Icon size="24px" name="carbon:camera" />
+    </BaseAvatar>
+
+    <div class="flex flex-col gap-2 w-full">
+      <label class="text-sm text-neutral-700" for="test_uuid">Group name</label>
+      <div :style="{ '--input-width': '44px' }" class="flex items-center top-0 stick y w-full relative">
+        <input id="test_uuid" type="text" placeholder="Search..." class="w-full px-4 h-11 pr-3 bg-slate-100 border-slate-200 rounded-lg outline-none text-sm">
+      </div>
+    </div>
+  </div>
+</template>
