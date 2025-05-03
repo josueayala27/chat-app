27106497ab1fbafac89f3a66e1835adcd2e634f8
Josué Ayala <ayalavalencia11@gmail.com>
2025-05-03 17:04:52 +0000

    feat: enhance `SignIn` component with form field structure for email and password inputs, and update button labels

---
+++ app/components/base/FormField.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{ label: string, hint?: string }>()
+const slots = defineSlots<{ default: () => any, hint: () => any }>()
+</script>
+
+<template>
+  <div class="flex flex-col gap-1">
+    <div class="flex justify-between items-center">
+      <label class="text-sm text-slate-700">{{ label }}</label>
+
+      <slot v-if="slots.hint || hint" name="hint">
+        <BaseFont class="text-sm text-slate-400" content="Optional" />
+      </slot>
+    </div>
+
+    <slot />
+
+    <BaseFont class="text-sm text-red-500" content="This field is required." />
+  </div>
+</template>
--- app/components/get-started/SignIn.vue
+++ app/components/get-started/SignIn.vue
@@ -1,32 +1,42 @@
 <template>
-  <div class="w-[370px] flex flex-col gap-5 p-6 rounded-lg shadow-xl shadow-slate-950/7">
+  <div class="w-[390px] flex flex-col gap-5 p-6 rounded-lg shadow-xl shadow-slate-950/7">
     <div class="flex flex-col">
-      <BaseFont class="font-semibold" content="Welcome to Chat App" />
+      <BaseFont class="font-semibold" content="Sign In to Chat App" />
       <BaseFont class="text-sm text-slate-700" content="Sign in to securely access your chats and stay connected with your friends and groups." />
     </div>
 
     <div class="flex flex-col gap-3">
-      <BaseInput
-        icon="carbon:user-avatar"
-        :ui="{ base: 'border border-slate-950/10 focus:border-sky-500' }"
-        placeholder="Email"
-      />
+      <BaseFormField label="Email">
+        <BaseInput
+          :ui="{ base: 'border border-slate-950/10 focus:border-sky-500' }"
+          icon="carbon:user-avatar"
+          placeholder="Enter email"
+        />
+      </BaseFormField>
 
-      <BaseInput
-        icon="carbon:password"
-        :ui="{ base: 'border border-slate-950/10 focus:border-sky-500' }"
-        type="password"
-        placeholder="Password"
-      />
+      <BaseFormField label="Password">
+        <BaseInput
+          :ui="{ base: 'border border-slate-950/10 focus:border-sky-500' }"
+          icon="carbon:password"
+          type="password"
+          placeholder="Enter password"
+        />
 
-      <BaseButton type="submit" content="Continue with email" />
+        <template #hint>
+          <NuxtLink to="/forgot-password" class="text-sm text-sky-500 font-medium">
+            Forgot password?
+          </NuxtLink>
+        </template>
+      </BaseFormField>
+
+      <BaseButton type="submit" content="Sign In" />
     </div>
 
     <div class="border-b border-slate-200" />
 
     <BaseButton type="button" :ui="{ base: 'gap-2' }">
       <Icon size="20px" name="grommet-icons:google" />
-      Sign In with Google
+      Continue with Google
     </BaseButton>
   </div>
 </template>
