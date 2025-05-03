c4a3cc24da103979c3bb634f221284a51bfe0235
Josué Ayala <ayalavalencia11@gmail.com>
2025-05-03 23:48:17 +0000

    feat: refactor SignIn component layout; enhance styling and structure for better user experience

--- app/components/auth/SignIn.vue
+++ app/components/auth/SignIn.vue
@@ -31,44 +31,37 @@ async function onSubmit() {
 </script>
 
 <template>
-  <div class="w-[390px] flex flex-col gap-5 p-6 rounded-lg shadow-xl shadow-slate-950/7">
-    <div class="flex flex-col">
-      <BaseFont class="font-semibold" content="Sign In to Chat App" />
-      <BaseFont class="text-sm text-slate-700" content="Sign in to securely access your chats and stay connected with your friends and groups." />
-    </div>
+  <div class="flex flex-col gap-3">
+    <BaseFormField name="email" label="Email">
+      <BaseInput
+        :ui="{ base: 'border border-slate-950/10 focus:border-sky-500' }"
+        icon="carbon:user-avatar"
+        placeholder="Enter email"
+      />
+    </BaseFormField>
 
-    <div class="flex flex-col gap-3">
-      <BaseFormField name="email" label="Email">
-        <BaseInput
-          :ui="{ base: 'border border-slate-950/10 focus:border-sky-500' }"
-          icon="carbon:user-avatar"
-          placeholder="Enter email"
-        />
-      </BaseFormField>
+    <BaseFormField name="password" label="Password">
+      <BaseInput
+        :ui="{ base: 'border border-slate-950/10 focus:border-sky-500' }"
+        icon="carbon:password"
+        type="password"
+        placeholder="Enter password"
+      />
 
-      <BaseFormField name="password" label="Password">
-        <BaseInput
-          :ui="{ base: 'border border-slate-950/10 focus:border-sky-500' }"
-          icon="carbon:password"
-          type="password"
-          placeholder="Enter password"
-        />
+      <template #hint>
+        <NuxtLink to="/forgot-password" class="text-sm text-sky-500 font-medium">
+          Forgot password?
+        </NuxtLink>
+      </template>
+    </BaseFormField>
 
-        <template #hint>
-          <NuxtLink to="/forgot-password" class="text-sm text-sky-500 font-medium">
-            Forgot password?
-          </NuxtLink>
-        </template>
-      </BaseFormField>
-
-      <BaseButton type="submit" content="Sign In" @click="onSubmit()" />
-    </div>
-
-    <div class="border-b border-slate-200" />
-
-    <BaseButton type="button" :ui="{ base: 'gap-2' }">
-      <Icon size="20px" name="grommet-icons:google" />
-      Continue with Google
-    </BaseButton>
+    <BaseButton type="submit" content="Sign In" @click="onSubmit()" />
   </div>
+
+  <div class="border-b border-slate-200" />
+
+  <BaseButton type="button" :ui="{ base: 'gap-2' }">
+    <Icon size="20px" name="grommet-icons:google" />
+    Continue with Google
+  </BaseButton>
 </template>
--- app/pages/sign-in.vue
+++ app/pages/sign-in.vue
@@ -3,7 +3,10 @@ definePageMeta({ layout: 'get-started' })
 </script>
 
 <template>
-  <div class="h-screen grid place-items-center">
-    <GetStartedSignIn />
-  </div>
+  <AuthContainer
+    title="Sign In to Chat App"
+    description="Sign in to securely access your chats and stay connected with your friends and groups."
+  >
+    <AuthSignIn />
+  </AuthContainer>
 </template>
