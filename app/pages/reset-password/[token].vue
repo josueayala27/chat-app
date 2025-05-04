49d21dd2dc962f063003399fa9a40684b9eeea02
Josué Ayala <ayalavalencia11@gmail.com>
2025-05-04 23:33:17 +0000

    feat: update `SignUp` form name and implement Reset Password functionality; add `Token` component with validation

--- app/pages/reset-password/[token].vue
+++ app/pages/reset-password/[token].vue
@@ -8,15 +8,6 @@ useHead({ title: 'Sign In' })
     title="Sign In to Chat App"
     description="Sign in to securely access your chats and stay connected with your friends and groups."
   >
-    <AuthSignIn />
-
-    <template #footer>
-      <div class="border-b border-slate-200" />
-
-      <BaseButton type="button" :ui="{ base: 'gap-2' }">
-        <Icon size="20px" name="grommet-icons:google" />
-        Continue with Google
-      </BaseButton>
-    </template>
+    <AuthResetPasswordToken />
   </AuthContainer>
 </template>
