643af358cb788f10cac8d3acef9c97546c717cb1
Josué Ayala <ayalavalencia11@gmail.com>
2025-05-06 11:20:55 -0600

    feat: enhance `Container` and `Root` components; improve styling and structure for better layout and readability

--- app/components/base/menu/Container.vue
+++ app/components/base/menu/Container.vue
@@ -6,7 +6,7 @@ const props = defineProps<{ ui?: Partial<typeof ui.slots>, items?: BaseMenuItemP
 
 const ui = tv({
   slots: {
-    base: 'bg-white/90 rounded-lg p-1 flex flex-col backdrop-blur-[10px] border border-slate-200 z-50',
+    base: 'bg-white/90 rounded-lg p-1 flex flex-col backdrop-blur-[10px] border border-slate-200 z-50 shadow-lg',
   },
 })
 
--- app/components/chat/window/messages/Root.vue
+++ app/components/chat/window/messages/Root.vue
@@ -4,38 +4,44 @@ import { tv } from 'tailwind-variants'
 const isOwn = inject<boolean>('isOwn')
 
 const ui = tv({
-  slots: { root: 'relative group rounded-lg' },
+  slots: {
+    root: 'relative group flex items-center gap-2',
+    base: 'rounded-lg',
+  },
   variants: {
     isOwn: {
-      true: { root: 'bg-sky-500 text-white' },
-      false: { root: 'bg-slate-100' },
+      true: { base: 'bg-sky-500 text-white' },
+      false: { base: 'bg-slate-100' },
     },
   },
 })
 
-const { root } = ui({ isOwn })
+const { root, base } = ui({ isOwn })
 </script>
 
 <template>
-  <div :class="root()">
-    <BasePopover :config="{ placement: isOwn ? 'bottom-end' : 'bottom-start' }">
-      <div class="rounded-tr-lg absolute top-0 right-0 h-8 aspect-square flex items-center justify-center invisible group-hover:visible cursor-pointer bg-inherit">
-        <Icon name="carbon:chevron-down" />
-      </div>
+  <div :class="[root()]">
+    <div :class="[base()]">
+      <slot />
+    </div>
+    <div class="">
+      <BasePopover :config="{ placement: isOwn ? 'bottom-end' : 'bottom-start' }">
+        <div class="p-2 rounded-full bg-slate-100 hover:bg-slate-200 grid place-items-center cursor-pointer">
+          <Icon size="20px" name="carbon:overflow-menu-horizontal" class="flex shrink-0 duration-300" />
+        </div>
 
-      <template #content>
-        <BaseMenuContainer>
-          <BaseMenuItem icon="carbon:information" label="Message Info" />
-          <BaseMenuItem icon="carbon:reply" label="Reply" />
-          <BaseMenuItem icon="carbon:copy-file" label="Copy" />
-          <BaseMenuItem icon="carbon:text-new-line" label="Forward" />
-          <BaseMenuItem icon="carbon:pin" label="Pin" />
-          <BaseMenuItem icon="carbon:star" label="Star" />
-          <BaseMenuItem :ui="{ root: 'text-red-500' }" icon="carbon:trash-can" label="Delete" />
-        </BaseMenuContainer>
-      </template>
-    </BasePopover>
-
-    <slot />
+        <template #content>
+          <BaseMenuContainer>
+            <BaseMenuItem icon="carbon:information" label="Message Info" />
+            <BaseMenuItem icon="carbon:reply" label="Reply" />
+            <BaseMenuItem icon="carbon:copy-file" label="Copy" />
+            <BaseMenuItem icon="carbon:text-new-line" label="Forward" />
+            <BaseMenuItem icon="carbon:pin" label="Pin" />
+            <BaseMenuItem icon="carbon:star" label="Star" />
+            <BaseMenuItem :ui="{ root: 'text-red-500' }" icon="carbon:trash-can" label="Delete" />
+          </BaseMenuContainer>
+        </template>
+      </BasePopover>
+    </div>
   </div>
 </template>
