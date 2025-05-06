4b7d6f5d468176113b255723e8c50d09eaaf58d8
Josué Ayala <ayalavalencia11@gmail.com>
2025-05-06 15:00:38 -0600

    Refactor code structure for improved readability and maintainability

--- app/components/chat/window/messages/Group.vue
+++ app/components/chat/window/messages/Group.vue
@@ -16,6 +16,7 @@ const ui = tv({
     root: 'flex relative',
     flex: 'flex flex-col',
     avatar: 'absolute z-40 top-0',
+    title: 'text-sm text-slate-900 font-medium',
   },
   variants: {
     isOwn: {
@@ -23,17 +24,19 @@ const ui = tv({
         root: 'flex-row-reverse',
         flex: 'items-end',
         avatar: 'right-0',
+        title: 'pr-[calc(48px+8px)]',
       },
       false: {
         root: 'flex-row',
         flex: 'items-start',
         avatar: 'left-0',
+        title: 'pl-[calc(48px+8px)]',
       },
     },
   },
 })
 
-const { root, flex, avatar } = ui({ isOwn: props.isOwn })
+const { root, flex, avatar, title } = ui({ isOwn: props.isOwn })
 provide('isOwn', props.isOwn)
 </script>
 
@@ -42,7 +45,7 @@ provide('isOwn', props.isOwn)
     <BaseAvatar :ui="{ base: avatar() }" />
 
     <div :class="flex({ class: 'gap-1 flex-1' })">
-      <BaseFont class="text-sm text-slate-900 font-medium pl-[calc(48px+8px)]" :content="isOwn ? 'Tú' : 'Josué Ayala'" />
+      <BaseFont :class="title()" :content="isOwn ? 'Tú' : 'Josué Ayala'" />
 
       <div :class="flex({ class: 'gap-0.5 bg-orange-400 w-full' })">
         <template v-for="(msg, i) in messageComponents" :key="i">
