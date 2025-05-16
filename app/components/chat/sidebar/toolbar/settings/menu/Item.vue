e32f7bb79a8bca0e709da266036bdd94c5b0eaaa
Josué Ayala <ayalavalencia11@gmail.com>
2025-05-15 21:08:55 -0600

    feat: update Item component to improve icon rendering and styling

--- app/components/chat/sidebar/toolbar/settings/menu/Item.vue
+++ app/components/chat/sidebar/toolbar/settings/menu/Item.vue
@@ -7,9 +7,9 @@ defineProps<{ option: Option, active: boolean }>()
 <template>
   <div
     :class="[{ 'bg-slate-100': active }]"
-    class="p-2 hover:bg-slate-100 rounded-lg cursor-pointer flex items-center gap-2 text-slate-900"
+    class="px-2 h-9 hover:bg-slate-100 rounded-lg cursor-pointer flex items-center gap-2 text-slate-900"
   >
-    <Icon name="carbon:user-avatar" />
+    <Icon size="20px" :name="option.icon" />
     <BaseFont class="text-sm" :content="option.label" />
   </div>
 </template>
