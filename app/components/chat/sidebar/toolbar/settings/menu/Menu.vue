bf736a41b9ca39f4e046b726bf8827655d5b759f
Josué Ayala <ayalavalencia11@gmail.com>
2025-05-17 16:04:45 -0600

    feat: adjust padding in various settings components for improved layout

--- app/components/chat/sidebar/toolbar/settings/menu/Menu.vue
+++ app/components/chat/sidebar/toolbar/settings/menu/Menu.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-[200px] p-2 border-r border-slate-200 flex flex-col gap-1">
+  <div class="w-[200px] p-3 border-r border-slate-200 flex flex-col gap-1">
     <slot />
   </div>
 </template>
