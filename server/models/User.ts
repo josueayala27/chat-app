c26bf7d30bb0554df51c6702cb2a7b54453b0aa2
Josué Ayala <ayalavalencia11@gmail.com>
2025-05-18 22:05:41 -0600

    feat: update `UserSchema` to include custom timestamps for `created_at` and `updated_at`

--- server/models/User.ts
+++ server/models/User.ts
@@ -5,6 +5,8 @@ const fields = {
   email: { type: String, required: true, unique: true },
 }
 
-const UserSchema = new mongoose.Schema(fields, { timestamps: true })
+const UserSchema = new mongoose.Schema(fields, {
+  timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+})
 
 export default mongoose.models.User || mongoose.model('User', UserSchema)
