c487cb625b8b698f9c89cb607c72cf3e16448ba8
Josué Ayala <ayalavalencia11@gmail.com>
2025-05-21 18:06:52 +0000

    refactor: remove default type from ReadBy and Message interfaces for improved type flexibility

--- shared/types/Message.d.ts
+++ shared/types/Message.d.ts
@@ -1,16 +1,14 @@
-import type { Types } from 'mongoose'
-
-export interface ReadBy<T = string | Types.ObjectId> {
+export interface ReadBy<T> {
   user_id: T
   read_at: Date
 }
 
-export interface Message<T = string | Types.ObjectId> {
+export interface Message<T> {
   chat_id: T
   sender_id: T
   content: string
   type: 'text' | 'image' | 'video' | 'file' | 'audio' | 'system'
   attachments?: string[]
-  reply_to?: string
+  reply_to?: T
   read_by?: ReadBy<T>[]
 }
