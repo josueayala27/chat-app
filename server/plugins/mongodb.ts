4eb8755b2cf8f868b9cac133db5ae189fc5e1707
Josué Ayala <ayalavalencia11@gmail.com>
2025-05-24 14:49:11 -0600

    fix(mongodb): update MongoDB URI construction to use runtime config instead of process.env

--- server/plugins/mongodb.ts
+++ server/plugins/mongodb.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export default defineNitroPlugin(async () => {
   const config = useRuntimeConfig()
-  const mongoUri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/${process.env.MONGODB_DB}?retryWrites=true&w=majority`
+  const mongoUri = `mongodb+srv://${config.public.MONGODB_USERNAME}:${config.public.MONGODB_PASSWORD}@${config.public.MONGODB_HOST}/${config.public.MONGODB_DB}?retryWrites=true&w=majority`
 
   if (!mongoUri) {
     console.warn('⚠️ No mongodbUri found in `runtimeConfig`.')
