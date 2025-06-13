031403eabdfa6f7f251f6f00ae3eec2c96a7fc58
Josué Ayala <ayalavalencia11@gmail.com>
2025-06-13 10:27:42 -0600

    feat: add scroll animation to chat window on mount

--- app/components/chat/window/Main.vue
+++ app/components/chat/window/Main.vue
@@ -1,5 +1,23 @@
+<script lang="ts" setup>
+import { gsap } from 'gsap'
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin'
+
+gsap.registerPlugin(ScrollToPlugin)
+
+const main = ref<HTMLElement>()
+
+onMounted(() => {
+  if (main.value) {
+    gsap.to(main.value, {
+      scrollTo: { y: 'max' },
+      duration: 0,
+    })
+  }
+})
+</script>
+
 <template>
-  <div class="p-2 overflow-auto scrollbar-hidden flex-1 flex flex-col gap-2">
+  <div ref="main" class="p-2 overflow-auto scrollbar-hidden flex-1 flex flex-col gap-2">
     <slot />
   </div>
 </template>
