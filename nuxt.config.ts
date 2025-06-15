d64f2318ec9dd58eb0df49dcfa495036f4413485
Josué Ayala <ayalavalencia11@gmail.com>
2025-06-14 18:52:37 -0600

    feat: add AWS S3 integration for generating signed URLs for file uploads

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,6 +36,7 @@ export default defineNuxtConfig({
     SUPABASE_SERVICE_ROLE: process.env.SUPABASE_SERVICE_ROLE,
     AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
     AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
   },
   future: {
     compatibilityVersion: 4,
