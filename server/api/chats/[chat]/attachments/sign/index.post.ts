d43372dc34dd35e09afda85c9a0cfc5ef0a7c8db
Josué Ayala <ayalavalencia11@gmail.com>
2025-06-14 19:07:08 -0600

    feat: simplify AWS S3 signed URL generation by using utility functions

--- server/api/chats/[chat]/attachments/sign/index.post.ts
+++ server/api/chats/[chat]/attachments/sign/index.post.ts
@@ -1,23 +1,11 @@
-import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 import { attachmentCreateSchema } from '~~/server/validators/attachment.validator'
 
 export default defineEventHandler(async (event) => {
   const body = await readValidatedBody(event, attachmentCreateSchema.parse)
+  const { createClient, createSignedUploadURL } = useAws()
 
-  const config = useRuntimeConfig()
-
-  const client = new S3Client({
-    region: 'us-east-1',
-    credentials: {
-      accessKeyId: config.AWS_ACCESS_KEY_ID,
-      secretAccessKey: config.AWS_SECRET_ACCESS_KEY,
-    },
-  })
-
-  const command = new PutObjectCommand({ Bucket: config.AWS_BUCKET_NAME, Key: body.filename })
-
-  const url = await getSignedUrl(client, command, { expiresIn: 3600 })
+  const client = createClient()
+  const url = await createSignedUploadURL(client, body.filename)
 
   return { upload_url: url }
 })
