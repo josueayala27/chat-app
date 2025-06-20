2b73108a603be47c131767b9024076380aafdd92
Josué Ayala <ayalavalencia11@gmail.com>
2025-06-20 12:09:47 -0600

    feat: remove unused dependencies and update package-lock.json

--- app/components/chat/window/messages/type/Text.vue
+++ app/components/chat/window/messages/type/Text.vue
@@ -1,5 +1,4 @@
 <script lang="ts">
-import MarkdownIt from 'markdown-it'
 import { tv } from 'tailwind-variants'
 
 interface WindowMessagesTypeTextProps {
@@ -10,28 +9,13 @@ interface WindowMessagesTypeTextProps {
 <script lang="ts" setup>
 defineProps<WindowMessagesTypeTextProps>()
 
-const ui = tv({ slots: { root: 'py-2 px-3 max-w-[32rem] text-sm' } })
+const ui = tv({ slots: { root: 'py-2 px-3 max-w-[32rem] text-sm [&>ul]:list-inside [&>ol]:list-inside [&>ul]:list-disc [&>ol]:list-decimal overflow-hidden' } })
 
 const { root } = ui()
-
-const md = new MarkdownIt({
-  // html: false,
-  // linkify: false,
-  // typographer: false,
-  breaks: true,
-}).enable('heading')
-
-// .disable(['image', 'link', 'blockquote', 'list', 'heading', 'table'])
-
-const html = md.render(
-  `- hola
-  
-1. Hola
-3. Hello`,
-)
 </script>
 
 <template>
-  <!-- rounded-r-lg last:rounded-bl-lg -->
-  <div :class="root()" v-html="html" />
+  <div :class="root()">
+    <BaseFont :content />
+  </div>
 </template>
