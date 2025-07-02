93a93fe083096342871384e08d9ad1aedfccca25
Josué Ayala <ayalavalencia11@gmail.com>
2025-07-01 21:12:58 -0600

    refactor: enhance layout and styling in File.vue and Media.vue components

--- app/components/chat/window/footer/type/default/preview/Media.vue
+++ app/components/chat/window/footer/type/default/preview/Media.vue
@@ -8,12 +8,14 @@ const emit = defineEmits<{ (e: 'remove'): void }>()
 <template>
   <div class="size-21 bg-slate-100 rounded-lg overflow-hidden shrink-0 relative group cursor-pointer">
     <img v-if="entry.src" :src="entry.src" class="w-full h-full object-cover">
+
     <div
       v-if="entry.status === 'uploading'"
       class="absolute top-0 left-0 w-full h-full bg-slate-900/50 grid place-items-center"
     >
       <Icon size="24px" name="svg-spinners:3-dots-fade" class="text-neutral-50" />
     </div>
+
     <div
       v-else
       class="absolute top-0 left-0 w-full h-full bg-slate-900/50 grid place-items-center invisible group-hover:visible"
